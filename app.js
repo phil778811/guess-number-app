25a0d89c3f91104d48d0687a5e57662256ab4845
Phil <s24586048@gmail.com>
2021-06-19 15:09:35 +0800

    add app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const numberImput =document.getElementById('number');
+const numberInput =document.getElementById('number');
 const btn =document.getElementById('btn');
 const randomNumber = Math.floor(Math.random()*6)+1;
-
+console.log(randomNumber);
 btn.addEventListener('click',function(){
     const userVal = parseInt(numberInput.value,10);
     
@@ -10,7 +10,7 @@ btn.addEventListener('click',function(){
         return;
     }
     
-    if(useVal>randomNumber){
+    if(userVal>randomNumber){
         alert('smaller');
         return;
     }
